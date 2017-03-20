34d0ed049295d895fe3c471e01e4b25b72e894bb
Prashanth <prashanth@slice.com>
2017-03-20 00:15:55 -0700

    Add express peer server

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ import express from  'express'
 import http from 'http'
 import path from 'path'
 import config from 'config'
+import { ExpressPeerServer } from 'peer'
 import _debug from 'debug'
 var debug = _debug('server')
 
@@ -13,6 +14,7 @@ let server = async () => {
   app.use(function (req, res) {
     res.sendfile(path.join(__dirname, '/../../build', 'index.html'))
   })
+  app.use('/peer', ExpressPeerServer(server, { debug: true }))
   httpServer.listen(9095, function () {
     debug('Server running on 9095')
   })

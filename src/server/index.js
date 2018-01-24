f3a8bbc6dc6a9f36134d856397280169f9c52904
Prashanth <prashanth@slice.com>
2018-01-24 00:13:01 -0800

    deploy: Update nginx config, add certbot, fix landing page

--- src/server/index.js
+++ src/server/index.js
@@ -16,7 +16,7 @@ var app = express()
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({limit: '0.5mb'}))
-// app.use(express.static(path.join(__dirname, '/../../build')))
+app.use(express.static(path.join(__dirname, '/../../build')))
 app.use('/', publicRouter)
 
 // API

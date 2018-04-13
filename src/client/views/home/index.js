2b25ff7544042b62efa15c813482d5ec84711369
Prashanth <prashanth@slice.com>
2018-04-12 18:49:18 -0700

    fix home

--- src/client/views/home/index.js
+++ src/client/views/home/index.js
@@ -3,12 +3,10 @@ import './index.css'
 
 export default class App extends Component {
   render () {
-    console.log('here')
     return (
       <div className='App'>
-        Welcome to talk
         <div className='App-header'>
-          <h2>Welcome to Talk to Me</h2>
+          <h2>Talk to Me</h2>
         </div>
         {/* <CreateRoom /> */}
       </div>

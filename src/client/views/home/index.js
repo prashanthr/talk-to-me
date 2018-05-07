0366629ebcf816240d9a5251d03583b4936e07a2
Prash <prashanth@slice.com>
2018-05-07 14:00:34 -0700

    add logo

--- src/client/views/home/index.js
+++ src/client/views/home/index.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 import InviteWrapper from '../../ui-components/invite-code/invite-wrapper'
+import Logo from '../../ui-components/logo'
+import { injectGlobal } from 'styled-components'
+import { getThemeCSS } from '../../utils/theme'
 import './index.css'
+// eslint-disable-next-line
+injectGlobal`
+body {
+  font-family: Helvetica, Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  ${getThemeCSS()}
+}
+`
 
 export default class App extends Component {
   render () {
     return (
       <div className='home'>
+        <Logo />
         <div className='splash'>
           {'Welcome'}
           <InviteWrapper />

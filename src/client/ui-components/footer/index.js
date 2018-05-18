8fe1ae9ecc5af11679965194cc1991a96f71f644
Prash <prashanth@slice.com>
2018-05-18 10:26:57 -0700

    update sentry and stun config

--- src/client/ui-components/footer/index.js
+++ src/client/ui-components/footer/index.js
@@ -5,7 +5,7 @@ const APP_VERSION = process.env.APP_VERSION
 const Footer = () => (
   <footer className='footer'>
     Copyright © PR. {`${new Date().getFullYear()}`}
-    {`${APP_VERSION ? `v${APP_VERSION}` : ''}`}
+    {`${APP_VERSION ? `. v${APP_VERSION}` : ''}`}
   </footer>
 )
 

24d263cbe9e05b8860682db494843ca326c50fd0
Prashanth R <prashanthrdev@gmail.com>
2020-03-17 13:54:05 -0700

    change footer impl

--- src/client/ui-components/footer/index.js
+++ src/client/ui-components/footer/index.js
@@ -5,14 +5,14 @@ import './index.css'
 const APP_VERSION = process.env.APP_VERSION
 
 const Footer = () => (
-  <footer className='footer'>
+  <div className='footer-div'>
     Copyright © PR {`${new Date().getFullYear()}. `} 
     {`${APP_VERSION ? `v${APP_VERSION}. ` : ''}`}
     Made with 💛
     <br />
     <a href={config.contactUrl}>Feedback/Contact. </a>
     <a href={config.bugUrl}>Report an issue</a>
-  </footer>
+  </div>
 )
 
 Footer.propTypes = {}

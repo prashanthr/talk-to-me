6de48ec9971a1dba55226e423e46b0e6a7650d97
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-07-02 17:38:56 -0700

    chore(ch71): Upgrades for babel

--- bin/server.sh
+++ bin/server.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-DEBUG=*,socket*,-babel,-engine:polling,-engine:ws,-engine,-express* ./node_modules/.bin/babel-node src/server/index.js
+DEBUG=*,socket*,-babel*,-engine:polling,-engine:ws,-engine,-express* ./node_modules/.bin/babel-node src/server/index.js

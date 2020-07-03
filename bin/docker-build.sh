2d91297f3971cb341f029a53eb5e6a85c4c66983
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-07-03 11:25:30 -0700

    fix up dockerfile and build script

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -4,5 +4,11 @@ if [ -z $TAG ]; then
   TAG="talk-to-me:dev"
 fi
 
+# config setup
+cp ./config/secrets/config/index.js ./src/client/config/
+
 echo "Building docker image with tag $TAG..."
 docker build -t $TAG .
+
+# Reset config
+git checkout ./src/client/config/

0c4c4596fe3eda4511d4c2f1450f01b892d214a6
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-07-04 14:08:56 -0700

    feat(ch49): Create k8 infra configs (#66)
    Co-authored-by: Prashanth R <1409501+prashanthr@users.noreply.github.com>

---
+++ deploy/kubernetes/scripts/deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+kubectl --namespace $KUBE_NAMESPACE apply -f $SCRIPTS_DIR/../namespace
+kubectl --namespace $KUBE_NAMESPACE apply -f $SCRIPTS_DIR/../deployment
+kubectl --namespace $KUBE_NAMESPACE get services

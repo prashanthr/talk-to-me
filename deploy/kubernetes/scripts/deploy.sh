2655e3ff523c8732e7b309b53cd6ae6f1afb07eb
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-07-14 17:41:35 -0700

    update deploy k8 script

--- deploy/kubernetes/scripts/deploy.sh
+++ deploy/kubernetes/scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-kubectl --namespace $KUBE_NAMESPACE apply -f $SCRIPTS_DIR/../namespace
+#kubectl --namespace $KUBE_NAMESPACE apply -f $SCRIPTS_DIR/../namespace
+#kubectl --namespace $KUBE_NAMESPACE apply -f $SCRIPTS_DIR/../secret
 kubectl --namespace $KUBE_NAMESPACE apply -f $SCRIPTS_DIR/../deployment
 kubectl --namespace $KUBE_NAMESPACE get services
